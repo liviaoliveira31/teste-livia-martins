1828ac7dae45538b6f54a19b34ffa65822f5a4e8
Livia Oliveira <livia.oliveira3104@outlook.com>
2025-03-25 12:26:38 -0300

    separacao de camadas e criacao de handler

---
+++ Theatrical.Players.Api/Controllers/TheatricalPlayersController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TheatricalPlayersRefactoringKata.Commands.PrintInvoice;
+
+namespace Theatrical.Players.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TheatricalPlayersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TheatricalPlayersController(
+            IMediator mediator
+        ) =>       
+            _mediator = mediator;
+        
+
+        [HttpPost("create")]
+        public async Task<PrintInvoiceCommandResponse> Post() 
+        {
+            return await _mediator.Send(new PrintInvoiceCommand());
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata/Commands/PrintInvoice/PrintInvoiceCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Commands.PrintInvoice
+{
+    public class PrintInvoiceCommandHandler : IRequestHandler<PrintInvoiceCommand, PrintInvoiceCommandResponse>
+    {
+        public async Task<PrintInvoiceCommandResponse> Handle(PrintInvoiceCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult( new PrintInvoiceCommandResponse());
+        }
+    }
+}
